5531bbe48950a4b5d63f1f12e1f258e67d07b8b0
Victor Kingi <kingivictor77@gmail.com>
2022-04-09 18:11:25 +0100

    scripting almost done

--- create_start_scripts.sh
+++ create_start_scripts.sh
@@ -8,7 +8,7 @@ do
     http=$(expr 46 + $i);
     addr=$(cat accounts.txt | sed $i!d accounts.txt)
     touch "start_node_$i.sh"; echo "#! /bin/bash" > "start_node_$i.sh";
-    text="geth/geth --networkid 9984 --datadir \"ethereum/node$i/data\" --bootnodes ${enode} --port 3030$port --ipcdisable --syncmode full --http --allow-insecure-unlock --http.corsdomain \"*\" --http.port 85$http --unlock 0x$addr --password \"ethereum/node$i/password.txt\" --mine console"
+    text="geth/geth --networkid 9984 --datadir \"ethereum/node$i/data\" --bootnodes ${enode} --port 3030$port --ipcdisable --syncmode full --http --allow-insecure-unlock --http.corsdomain \"*\" --http.port 85$http --ws --ws.origins \"*\" --unlock 0x$addr --password \"ethereum/node$i/password.txt\" --mine console"
     echo ${text} >> "start_node_$i.sh";
     chmod ugo+x "start_node_$i.sh"
 
